30adc4a4eb79f9eefa0653b2f6e90e7ad8434dad
mmmmnd <handsome_mo@126.com>
2022-04-27 17:37:39 +0800

    调整业务逻辑

--- src/com/player/model/Menu.java
+++ src/com/player/model/Menu.java
@@ -1,7 +1,9 @@
 package com.player.model;
 
 public class Menu {
-
+  /**
+   * 主菜单
+   */
   public void mainMenu() {
     System.out.println("**************************");
     System.out.println("        **主菜单**");
@@ -11,15 +13,34 @@ public class Menu {
     System.out.println("**************************");
   }
 
-  public Boolean closeMenu(int i) {
-    switch (i) {
-      case 1:
-        System.out.println(1);
-        break;
-      case 2:
-        System.out.println(2);
-        break;
-    }
-    return false;
+  /**
+   * 列表菜单
+   */
+  public void listMenu() {
+    System.out.println("*********************************************");
+    System.out.println("           **播放器列表管理**");
+    System.out.println("           1--将歌曲添加到主播放列表");
+    System.out.println("           2--将歌曲添加到普通播放列表");
+    System.out.println("           3--通过歌曲id查询播放列表中的歌曲");
+    System.out.println("           4--通过歌曲名称查询播放列表中的歌曲");
+    System.out.println("           5--修改播放列表中的歌曲");
+    System.out.println("           6--删除播放列表中的歌曲");
+    System.out.println("           7--显示播放列表中的所有歌曲");
+    System.out.println("           9--返回上一级菜单");
+    System.out.println("*********************************************");
+  }
+
+  /**
+   * 播放器菜单
+   */
+  public void player(){
+    System.out.println("*********************************************");
+    System.out.println("           **播放器管理**");
+    System.out.println("           1--向播放器添加播放列表");
+    System.out.println("           2--从播放器删除播放列表");
+    System.out.println("           3--通过名字查询播放列表信息");
+    System.out.println("           4--显示所有播放列表名称");
+    System.out.println("           9--返回上一级菜单");
+    System.out.println("*********************************************");
   }
 }
--- src/com/player/test/MainTest.java
+++ src/com/player/test/MainTest.java
@@ -9,21 +9,44 @@ public class MainTest {
   public static void main(String[] args) {
     Menu menu = new Menu();
     Scanner scanner = new Scanner(System.in);
-    int inputIndex = 0;
+    int inputIndex = 0; //输入索引
+    boolean flag = true, flagList = true, flagPlayer = true;// flag 主菜单 flagList 列表菜单 flag Player 播放器菜单
 
-    while (true) {
+    while (flag) {
       menu.mainMenu();
       try {
+        System.out.println("请输入对应数字进行操作");
         inputIndex = scanner.nextInt();
       } catch (InputMismatchException e) {
         System.out.println("输入的格式有误请重新输入！");
         scanner.next();
+        continue;
       }
 
-      if(inputIndex == 0){
-        return;
-      }
+      switch (inputIndex) {
+        case 0:
+          flag = false;
+        case 1:
+          while (flagList) {
+            menu.listMenu();
+            System.out.println("请输入对应的数字对播放列表进行管理:");
+            try {
+              inputIndex = scanner.nextInt();
+            } catch (InputMismatchException e) {
+              System.out.println("输入的格式有误请重新输入！");
+              scanner.next();
+              continue;
+            }
+            if (inputIndex == 9) flagList = false;
 
+          }
+          break;
+        case 2:
+          menu.player();
+          break;
+        default:
+          System.out.println("输入有误请重新输入！");
+      }
     }
   }
 }
